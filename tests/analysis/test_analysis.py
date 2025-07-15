bd4490f0686c8d72a7f40450c24d6aceaf40bad4
Alexander Bonkowski <alexander.bonkowski@rwth-aachen.de>
2025-07-15 14:03:22 +0200

    ignore `PLC0415`

--- tests/analysis/test_analysis.py
+++ tests/analysis/test_analysis.py
@@ -63,7 +63,7 @@ class TestGBStructure(TestCase):
 
     def test_grain_segmentation(self) -> None:
         """Test the grain segmentation method."""
-        from ovito.modifiers import GrainSegmentationModifier
+        from ovito.modifiers import GrainSegmentationModifier  # noqa: PLC0415
 
         self.data.perform_ptm(enabled=("fcc"), output_orientation=True)
         self.data.get_distinct_grains(compute=False)
