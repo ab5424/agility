c8465001f6280569daf37e67f07c20ae339cb733
Alexander Bonkowski <alexander.bonkowski@rwth-aachen.de>
2026-03-14 13:17:15 +0100

    Update agility/analysis.py
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- agility/analysis.py
+++ agility/analysis.py
@@ -85,8 +85,10 @@ class GBStructure:
             from ase.io import read  # noqa: PLC0415
 
             self.data = types.SimpleNamespace()
-            self.data.atoms = read(str(filename), **kwargs)
+            self.data.atoms = read(str(filename))
             self.data.selection = []
+
+        elif self.backend == "pymatgen":
 
         if self.backend == "pymatgen":
             from pymatgen.core import Structure  # noqa: PLC0415
